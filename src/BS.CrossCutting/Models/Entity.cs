43caa9e4a17ab1b82bb286978271ef2e6296b005
Robson Maia <robsonmvieira@gmail.com>
2020-12-28 13:57:08 -0300

    :sparkles: add Entity base class

---
+++ src/BS.CrossCutting/Models/Entity.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace BS.Domain.Models
+{
+    public abstract class Entity
+    {
+        public Guid Id { get; set; }
+
+        protected Entity()
+        {
+            Id = Guid.NewGuid();
+        }
+    }
+}
